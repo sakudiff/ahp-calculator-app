363bcbc0dca7b5431d9d9365ebac6531d05f53f3
sakudiff <shawvirnn@gmail.com>
2025-07-23 21:50:21 +0800

    fix: explicitly set build output directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/ahp-calculator-app/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 })
